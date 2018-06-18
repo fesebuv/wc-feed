724595413902fc1d7d168b7b96bac90ff3ea0cc1
Felipe Buenaño <fesebuv@gmail.com>
2018-06-18 14:53:01 -0400

    add pagination capabilities

--- src/server/handlers/index.js
+++ src/server/handlers/index.js
@@ -12,30 +12,28 @@ const tags = [
 ];
 
 function feed (request, response) {
+  const { query } = request;
+  const { page = 1} = query;
   const tagString = tags.join(', ');
   const params = {
-    'method': 'flickr.photos.search',
-    'page' : 1,
-    'tags': tagString,
-    'text': tagString,
-    'per_page':'10',
-    'extras': 'url_o, url_s, url_n, url_z',
-    'media': 'photos',
-    'api_key': process.env.FLICKR_KEY,
-    'format': 'json',
-    'nojsoncallback': '?'
+    method: 'flickr.photos.search',
+    page : page,
+    tags: tagString,
+    text: tagString,
+    per_page:'10',
+    extras: 'url_o, url_s, url_n, url_z',
+    media: 'photos',
+    api_key: process.env.FLICKR_KEY,
+    format: 'json',
+    nojsoncallback: '?'
   };
   const uri = `${FLICKR_CONSTANTS.endpoint}?${querystring.stringify(params)}`;
   fetch(uri)
-  .then(data => data.json())
-  .then(data => {
-    console.log(JSON.stringify(data, null, 2));
-    return data;
-  })
-  .then((json) => response.send(json))
-  .catch((err) => {
-    console.log(err);
-  });
+    .then(data => data.json())
+    .then((json) => response.send(json))
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 module.exports = {
