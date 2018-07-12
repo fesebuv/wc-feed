fbd8280b563423f8f08c61a3d938473b5bcdae54
Felipe Buenaño <fesebuv@gmail.com>
2018-07-12 12:39:14 -0400

    introduced vue

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,27 +1,28 @@
 
-require('whatwg-fetch');
+const whatwgFetch = require('whatwg-fetch');
 const querystring = require('querystring');
+const { default: Vue }  = require('vue');
 
 var PAGE = 0;
 
-function parseData (data) {
-  var fragment = document.createDocumentFragment();
-  var photos = data.photos.photo || [];
-  photos.forEach(function (photo) {
-    var src = photo.url_o || photo.url_n;
-    var img = document.createElement('img');
-    img.src = src;
-    img.alt = src;
-    fragment.appendChild(img);
-  });
-  return fragment;
-}
-
-function response (data) {
-  var fragment = parseData(data);
-  var root = document.getElementById('root');
-  root.appendChild(fragment);
-}
+// function parseData (data) {
+//   var fragment = document.createDocumentFragment();
+//   var photos = data.photos.photo || [];
+//   photos.forEach(function (photo) {
+//     var src = photo.url_o || photo.url_n;
+//     var img = document.createElement('img');
+//     img.src = src;
+//     img.alt = src;
+//     fragment.appendChild(img);
+//   });
+//   return fragment;
+// }
+//
+// function response (data) {
+//   var fragment = parseData(data);
+//   var root = document.getElementById('root');
+//   root.appendChild(fragment);
+// }
 
 function getResults () {
   PAGE = PAGE + 1;
@@ -31,17 +32,41 @@ function getResults () {
   const uri = `/feed?${querystring.stringify(params)}`;
   fetch(uri)
     .then((resp) => resp.json())
-    .then(response);
+    // .then(response);
+    .then(getImages)
 };
 
 (function () {
   getResults();
 })();
 
-window.getResults = getResults;
 window.addEventListener('scroll', function(evt) {
   const hasScrolled = (window.innerHeight + window.pageYOffset) >= document.body.offsetHeight;
   if (hasScrolled) {
-    window.getResults();
+    getResults();
+  }
+});
+
+
+function getImages (data) {
+  var photos = data.photos.photo || [];
+  photos.forEach(function (photo) {
+    var src = photo.url_o || photo.url_n;
+    app.imageList.push({
+      src: src
+    });
+  });
+}
+
+Vue.component('image-item', {
+  props: ['item'],
+  template: '<img v-bind:src="item.src"/>'
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    imageList: [],
+    message: 'Hello World Cup!'
   }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,33 @@ module.exports =  {
   output: {
     path: path.resolve(__dirname, './src/public/js'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: { }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
   }
 };
